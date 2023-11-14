1cb74dfe47d597729a6cd1afe7deb80e226dfc7f
Amer Kulovic <amer.kulovic@yahoo.com>
2023-11-14 12:59:18 -0600

    created an api call

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>Book Search</h2>
+    <input v-model="searchTerm" type="text" placeholder="Enter book name" />
+    <button @click="searchBooks">Search</button>
+
+    <div v-if="books.length">
+      <h3>Search Results</h3>
+      <ul>
+        <li v-for="book in books" :key="book.id">{{ book.volumeInfo.title }}</li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: "",
+      books: [],
+    };
+  },
+  methods: {
+    async searchBooks() {
+      try {
+        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.searchTerm}`);
+        const data = await response.json();
+        this.books = data.items || [];
+        console.log(data.items);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
+};
+</script>
