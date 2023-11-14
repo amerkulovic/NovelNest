7b0b7e4886407cf6719ea4854d71585893a25dd6
Amer Kulovic <amer.kulovic@yahoo.com>
2023-11-14 13:15:41 -0600

    created a default nav

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
-<template>
-  <div>
-    <h2>Book Search</h2>
-    <input v-model="searchTerm" type="text" placeholder="Enter book name" />
-    <button @click="searchBooks">Search</button>
-
-    <div v-if="books.length">
-      <h3>Search Results</h3>
-      <ul>
-        <li v-for="book in books" :key="book.id">{{ book.volumeInfo.title }}</li>
-      </ul>
-    </div>
-  </div>
-</template>
-
 <script>
+import Nav from "./components/Nav.vue";
+
 export default {
   data() {
     return {
       searchTerm: "",
       books: [],
     };
+  },
+  components: {
+    Nav,
   },
   methods: {
     async searchBooks() {
@@ -35,3 +25,19 @@ export default {
   },
 };
 </script>
+
+<template>
+  <Nav />
+  <div>
+    <h2 class="text-2xl">Book Search</h2>
+    <input v-model="searchTerm" type="text" placeholder="Enter book name" />
+    <button @click="searchBooks">Search</button>
+
+    <div v-if="books.length">
+      <h3>Search Results</h3>
+      <ul>
+        <li v-for="book in books" :key="book.id">{{ book.volumeInfo.title }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="flex flex-row justify-between items-center p-5 bg-orange-200">
+    <h1 class="text-4xl">NovelNest</h1>
+    <ul class="flex flex-row">
+      <li class="mx-2">Bookmarks</li>
+      <li>Search</li>
+    </ul>
+  </section>
+</template>
