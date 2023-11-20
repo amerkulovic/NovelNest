1778bd5596c5b1724c8e9284b7b40f6d40041996
Amer Kulovic <amer.kulovic@yahoo.com>
2023-11-20 14:15:44 -0600

    improved mobile screen of the bookpage

--- src/pages/BookPage.vue
+++ src/pages/BookPage.vue
@@ -7,11 +7,13 @@ const display = () => {
 };
 </script>
 <template>
-  <div class="bg-orange-200 flex items-center h-[550px]">
-    <div class="flex justify-center relative py-10">
-      <div class="bg-orange-300 h-[90%] w-[60%] absolute left-20 opacity-70 top-7 z-10"></div>
-      <img class="h-[403px] max-sm:h-[150px] w-[268px] z-20 border-2 border-yellow-900" :src="book.imageLinks.thumbnail" />
-      <section class="w-3/5 ml-5 flex flex-col justify-between z-20">
+  <div class="bg-orange-200 flex items-center pb-10 max-xl:pt-20">
+    <div class="flex justify-center relative py-10 max-md:flex-col max-md:items-center">
+      <div class="bg-orange-300 h-[90%] w-[60%] absolute left-20 opacity-70 top-7 max-lg:w-full max-lg:left-0 max-md:h-full"></div>
+      <div class="z-10 flex items-center">
+        <img class="h-[403px] w-[268px] border-2 border-yellow-900" :src="book.imageLinks.thumbnail" />
+      </div>
+      <section class="w-3/5 ml-5 flex flex-col justify-between z-10">
         <div>
           <h1 class="font-extrabold text-4xl py-2">{{ book.title }}</h1>
           <h2 :v-if="book.subtitle" class="font-extralight text-3xl pb-2">{{ book.subtitle }}</h2>
