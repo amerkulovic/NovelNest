a6bbe137c16be077ac0d90aabb35a105a7f63a9c
Amer Kulovic <amer.kulovic@yahoo.com>
2023-11-27 10:50:14 -0600

    added media queries to the bookcard component

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -9,18 +9,18 @@ const storeBookInfo = () => {
 </script>
 <template>
   <div class="flex justify-evenly my-3 bg-orange-300 w-5/6 py-3">
-    <div class="flex justify-center items-center">
-      <img :src="image" class="h-5/6 w-[200px] border-4 border-yellow-800" />
+    <div class="flex justify-center items-center max-md:mx-3">
+      <img :src="image" class="h-5/6 w-[200px] border-4 border-yellow-800 max-md:w-[175px] max-md:h-4/6" />
     </div>
     <section class="flex flex-col justify-between w-4/6 my-2">
       <section>
-        <h2 class="text-3xl font-bold mb-3 italic">{{ title }}</h2>
-        <p class="font-extralight h-[200px] overflow-hidden" style="overflow-y: auto"><span class="font-bold">Summary: </span>{{ desc }}</p>
+        <h2 class="text-3xl font-bold mb-3 italic max-sm:text-xl max-sm:mr-2">{{ title }}</h2>
+        <p class="font-extralight h-[200px] max-sm:mx-2 overflow-hidden max-md:h-[150px]" style="overflow-y: auto"><span class="font-bold">Summary: </span>{{ desc }}</p>
       </section>
-      <section class="flex flex-row justify-between items-center mt-8">
+      <section class="flex flex-row justify-between items-center mt-8 max-md:mt-4">
         <p class="font-extralight italic">Written by {{ author[0] }}</p>
         <router-link to="/book">
-          <button class="p-3 bg-yellow-950 text-white font-bold" @click="storeBookInfo">Read More</button>
+          <button class="p-3 bg-yellow-950 text-white font-bold max-md:p-2 max-md:mx-2 max-md:text-sm" @click="storeBookInfo">Read More</button>
         </router-link>
       </section>
     </section>
