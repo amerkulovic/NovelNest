b0e958a4e0c70d665080c1ba7080de052c57a6ef
Amer Kulovic <amer.kulovic@yahoo.com>
2024-02-21 12:13:12 -0600

    added transition effects to the book search page

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -10,12 +10,12 @@ const storeBookInfo = () => {
 <template>
   <div class="flex justify-evenly my-3 bg-orange-300 w-5/6 py-3">
     <div class="flex justify-center items-center max-md:mx-3">
-      <img :src="image" class="h-5/6 w-[200px] border-4 border-yellow-800 max-md:w-[175px] max-md:h-4/6" />
+      <img :src="image" class="h-5/6 w-[200px] border-4 border-yellow-800 max-md:w-[175px] max-md:h-4/6 transition-all duration-300" />
     </div>
     <section class="flex flex-col justify-between w-4/6 my-2">
       <section>
-        <h2 class="text-3xl font-bold mb-3 italic max-sm:text-xl max-sm:mr-2">{{ title }}</h2>
-        <p class="font-extralight h-[200px] max-sm:mx-2 overflow-hidden max-md:h-[150px]" style="overflow-y: auto"><span class="font-bold">Summary: </span>{{ desc }}</p>
+        <h2 class="text-3xl font-bold mb-3 italic max-sm:text-xl max-sm:mr-2 transition-all duration-300">{{ title }}</h2>
+        <p class="font-extralight h-[200px] max-sm:mx-2 overflow-hidden max-md:h-[150px] transition-all duration-300" style="overflow-y: auto"><span class="font-bold">Summary: </span>{{ desc }}</p>
       </section>
       <section class="flex flex-row justify-between items-center mt-8 max-md:mt-4">
         <p class="font-extralight italic">Written by {{ author[0] }}</p>
@@ -31,7 +31,7 @@ const storeBookInfo = () => {
 p {
   overflow-y: auto;
   scrollbar-width: thin;
-  scrollbar-color: #999 transparent;
+  scrollbar-color: #8d8d8d transparent;
 }
 
 p::-webkit-scrollbar {
